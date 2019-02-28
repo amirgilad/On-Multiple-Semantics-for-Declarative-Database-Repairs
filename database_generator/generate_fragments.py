29d656fc37d7fe0fee28dd13c707af47da2da3db
amirgilad <amirgilad2233@gmail.com>
2019-02-28 08:38:16 -0800

    change schema

--- database_generator/generate_fragments.py
+++ database_generator/generate_fragments.py
@@ -12,9 +12,9 @@ def create_tables(conn):
 
     create_queries_prov = [
         'CREATE TABLE author (aid int(11), name varchar(60), oid int(11), papar_count int(11), citation_count int(11));',
-        'CREATE TABLE publication (wid int(11), cid int(11), title varchar(100), citeid int(11), refid int(11));',
+        'CREATE TABLE publication (wid int(11), cid int(11), title varchar(100), year int(11));',
         'CREATE TABLE writes (aid int(11), wid int(11));',
-        'CREATE TABLE cite (citeid int(11), refid int(11));',
+        'CREATE TABLE cite (citing int(11), cited int(11));',
         'CREATE TABLE organization (oid int(11), name varchar(100));'
         ]
     for cq in create_queries_prov:
