dfe322e9b2652861a27f9d5aa0fb6f9bdf2fdb04
amirgilad <amirgilad2233@gmail.com>
2019-09-04 21:08:39 -0400

    Process query results into provenance for independent semantics.

--- Semantics/independent_sem.py
+++ Semantics/independent_sem.py
@@ -9,7 +9,10 @@ class IndependentSemantics(AbsSemantics):
     def __init__(self, db_conn, rules, tbl_names):
         super(IndependentSemantics, self).__init__(db_conn, rules, tbl_names)
 
-    def find_mss(self):
+        self.provenance = {}# dict in the form {delta tuple: [assign1, assign2, ...]}
+        self.prov_notations = {}# dict in the form {notation: tuple}
+
+    def find_mss(self, schema):
         """implementation of approximation algorithm for independent semantics.
         Store the provenance of all possible delta tuples as a CNF and find the
         smallest satisfying assignment using a SAT solver"""
@@ -20,18 +23,16 @@ class IndependentSemantics(AbsSemantics):
         self.db.load_database_tables(self.delta_tuples.keys(), is_delta=True)
 
         # convert the rules so they will store the provenance
-        prov_rules = self.convert_to_prov_rules()
-
-        # set to store the provenance
-        prov = set()
+        prov_rules, prov_tbls, proj = self.gen_prov_rules()
 
         # use end semantics to derive all delta tuples and store the provenance
         changed = True
         prev_len = 0
         while changed:
             for i in range(len(self.rules)):
-                cur_prov = self.db.execute_query(self.rules[i][1])
-                prov.update([(self.rules[i][0], row) for row in cur_prov])
+                cur_prov = self.db.execute_query(prov_rules[i][1])
+                assignments, res_tuple = self.rows_to_prov(self, cur_prov, prov_tbls[i], schema, proj, prov_rules[i])
+                self.provenance.update([(self.rules[i][0], row) for row in cur_prov])
                 changed = prev_len != len(mss)
                 prev_len = len(mss)
         return mss
@@ -39,15 +40,17 @@ class IndependentSemantics(AbsSemantics):
     def gen_prov_rules(self):
         """convert every rule to a rule that outputs the provenance"""
         prov_rules = []
-        for query in self.rules:
+        prov_tbls = []
+        for i in range(len(self.rules)):
+            query = self.rules[i]
             q_parts = query[1].lower().split("from")
             proj = q_parts[0].split('select')[1].strip().split(',')
             proj = [e.strip() for e in proj]
             rest = q_parts[1].split("where")
-            prov_tbls = rest[0].strip().split(',')
+            prov_tbls.append([tbl.strip() for tbl in rest[0].strip().split(',')])
             prov_proj = ""
             prov_lst = []
-            for tbl in prov_tbls:
+            for tbl in prov_tbls[i]:
                 if "as" in tbl:
                     prov_proj += tbl.split("as")[1] + ".*, "
                     prov_lst.append(tbl.split("as")[0][:-1].strip())
@@ -56,4 +59,41 @@ class IndependentSemantics(AbsSemantics):
             prov_proj = prov_proj[:-2]
             q_prov = "SELECT " + prov_proj + " FROM" + rest[0] + "WHERE" + rest[1]
             prov_rules.append((query[0], q_prov))
-        return prov_rules
+        return prov_rules, prov_tbls, proj
+
+    def handle_assignment(self, row, example_tuples, schema, prov_tbls, rule):
+        s = 0
+        str_row = [str(e) for e in row]
+        ans = ("", "")
+        for tbl in prov_tbls:
+            e = len(schema[tbl]) + s
+            attrs = ",".join(["'" + t + "'" if "\r" not in t else "'" + t[:-4] + "'" for t in str_row[s:e]])
+            txt_tbl = (tbl, "(" + attrs + ")")
+            # self.prov_notations[]
+            example_tuples.append(txt_tbl)
+            if rule[0] == txt_tbl[0]:
+                ans = ("delta_" + txt_tbl[0], txt_tbl[1])
+            s = e
+        return example_tuples, ans
+
+    def rows_to_prov(self, res, prov_tbls, schema, proj, rule):
+        # separate every result row into provenance tuples
+        proj_attrs = []
+        for p in proj:
+            t = tuple(p.split("."))
+            proj_attrs.append(t)
+        assignments = []
+        for i in range(len(res)):
+            example_tuples = []
+            row = res[i]
+            example_tuples, ans = self.handle_assignment(row, example_tuples, schema, prov_tbls, rule)
+            example_tuples = [ans] + example_tuples
+            assignments.append(example_tuples)
+        return assignments
+
+    def process_provenance(self, assignments):
+        # get the provenance of all tuples
+        for assign in assignments:
+            if assign[0] not in self.provenance:
+                self.provenance[assign[0]] = []
+            self.provenance[assign[0]].append(assign[1:])
--- test/test_independent_sem.py
+++ test/test_independent_sem.py
@@ -5,6 +5,35 @@ import unittest
 
 class TestIndependentSemantics(unittest.TestCase):
 
+    schema = {"author": ('aid',
+                         'name',
+                         'oid'),
+              "publication": ('pid',
+                              'title',
+                              'year'),
+
+              "writes": ('aid', 'pid'),
+
+              "organization": ('oid',
+                               'name'),
+
+              "conference": ('cid',
+                             'mas_id',
+                             'name',
+                             'full_name',
+                             'homepage',
+                             'paper_count',
+                             'citation_count',
+                             'importance'),
+
+              "domain_conference" : ('cid', 'did'),
+
+              "domain" : ('did',
+                          'name',
+                          'paper_count',
+                          'importance')
+              }
+
     def test_undefined_connection(self):
         # test no db connection
         rules = [("author", "SELECT * FROM author WHERE author.aid = 58525;")]
@@ -18,12 +47,13 @@ class TestIndependentSemantics(unittest.TestCase):
         tbl_names = ["organization", "author", "publication", "writes"]
         db = DatabaseEngine("cr")
         ind_sem = IndependentSemantics(db, rules, tbl_names)
-        mss = ind_sem.find_mss()
+        mss = ind_sem.find_mss(self.schema)
 
         self.assertEqual(mss, set(), "MSS supposed to be empty! Instead its " + str(mss))
 
     def test_gen_prov_rules(self):
-        rules = [("author", "SELECT author.name FROM author, writes WHERE author.name LIKE '%m%' AND author.aid = writes.aid;")]
+        # test func that takes delta rules and turns them to rules that return provenance
+        rules = [("author", "SELECT author.* FROM author, writes WHERE author.name LIKE '%m%' AND author.aid = writes.aid;")]
         tbl_names = ["organization", "author", "publication", "writes"]
         db = DatabaseEngine("cr")
 
@@ -32,10 +62,26 @@ class TestIndependentSemantics(unittest.TestCase):
         db.load_database_tables(tbl_names)
 
         ind_sem = IndependentSemantics(db, rules, tbl_names)
-        prov_rule = ind_sem.gen_prov_rules()[0][1]
+        prov_rule = ind_sem.gen_prov_rules()[0][0][1]
         func_prov_results = db.execute_query(prov_rule)
         desired_prov_results = db.execute_query("SELECT author.*, writes.* FROM author, writes WHERE author.name LIKE '%m%' AND author.aid = writes.aid;")
         self.assertEqual(func_prov_results, desired_prov_results)
+
+    def test_rows_to_prov(self):
+        # test func that handles and stores the provenance of each tuple
+        rules = [("author", "SELECT author.* FROM author, writes WHERE author.name LIKE '%m%' AND author.aid = writes.aid;")]
+        tbl_names = ["organization", "author", "publication", "writes"]
+        db = DatabaseEngine("cr")
+
+        # reset the database
+        db.delete_tables(tbl_names)
+        db.load_database_tables(tbl_names)
+
+        ind_sem = IndependentSemantics(db, rules, tbl_names)
+        prov_rules, prov_tbls, proj = ind_sem.gen_prov_rules()
+        cur_prov = db.execute_query(prov_rules[0][1])
+        assignments = ind_sem.rows_to_prov(cur_prov, prov_tbls[0], self.schema, proj, prov_rules[0])
+        print(assignments)
 
     def test_easy_case(self):
         # test case with one simple rule
@@ -50,12 +96,12 @@ class TestIndependentSemantics(unittest.TestCase):
         ind_sem = IndependentSemantics(db, rules, tbl_names)
 
         results = db.execute_query("SELECT * FROM author WHERE author.aid = 58525;")
-        mss = ind_sem.find_mss()
+        mss = ind_sem.find_mss(self.schema)
         mss_no_rel = [e[1] for e in mss]
         self.assertTrue(all(t in mss_no_rel for t in results))
 
     def test_hard_case(self):
-        # test case with one simple rule
+        # test case with two simple rules
         rules = [("author", "SELECT * FROM author WHERE author.name like '%m%';"), ("writes", "SELECT * FROM writes WHERE pid = 1270038;")]
         tbl_names = ["organization", "author", "publication", "writes"]
         db = DatabaseEngine("cr")
